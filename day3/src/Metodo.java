7b0fda716aabb55bb11e4e21d2d5e7a5ee60f3e0
Laia <laia@hidalgojimenez.es>
2021-11-12 10:19:57 +0100

    added arrangements

--- day3/src/Metodo.java
+++ day3/src/Metodo.java
@@ -16,9 +16,12 @@ public class Metodo {
                 System.out.println("Nombre: " + itemList.get(0) + "\nApellido1: " + itemList.get(1));
             } else if(itemList.size() == 2 && (NombresPila.nombres.contains(itemList.get(1)))) {
                 System.out.println("Nombre: " + itemList.get(0) + " " + itemList.get(1));
-            }else {
-                System.out.println("El nombre introducido no es correcto");
+            }else if(itemList.size() == 4 && (NombresPila.nombres.contains(itemList.get(1)))) {
+            System.out.println("Nombre: " + itemList.get(0) + " " + itemList.get(1) + "\nApellido1: " + itemList.get(2) + "\nApellido2: " + itemList.get(3));
+            }else{
+            System.out.println("El nombre introducido no es correcto");
+        }
             }
         }
-    }
+
 
